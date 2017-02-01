1f57d64c9b29e2980112f30376908649655b7863
Zeby95 <seba.fernandez95@gmail.com>
2017-02-01 18:50:04 -0300

    Fix on some issues.

--- twitter-bot.py
+++ twitter-bot.py
@@ -5,11 +5,11 @@ from datetime import datetime
 
 def sentence ():
 	art = ['El', 'La', 'Tu', 'Yo', 'Los', 'Las', 'Nosotros']
-	verbs = ['jugar', 'robar', 'saltar', 'cocinar', 'cortar', 'pintar']
-	adj = ['grande', 'gordo', 'chico', 'alto', 'hueco', 'profundo']
-	r1 = random.randrange (0, 7)
-	r2 = random.randrange (0, 6)
-	r3 = random.randrange (0, 6)
+	verbs = ['jugar', 'robar', 'saltar', 'cocinar', 'cortar', 'pintar', 'absorber', 'bendecir', 'freir', 'poseer', 'sujetar', 'decir', 'prender', 'imprimir']
+	adj = ['grande', 'gordo', 'chico', 'alto', 'hueco', 'profundo', 'amargo', 'muerto', 'mojado', 'dormido', 'feo']
+	r1 = random.randrange (0, len (art))
+	r2 = random.randrange (0, len (verbs))
+	r3 = random.randrange (0, len (adj))
 	phrase = art [r1] + ' ' + verbs [r2] + ' ' + adj [r3] + '.'
 	return phrase 
 
@@ -25,10 +25,19 @@ def write_id(id):
 	f.close()
 
 def log_tweet(x, now):
-	msj = "Tweet nro: " + str (x) + "			" + str (now.hour) + ":" + str (now.minute) 
+	msj = "Tweet nro: " + str (x) + "			" + now.strftime("%H:%M") 
 	print (" ")
 	print (msj)
-	
+
+def info_tweet (replyname, text, reply):
+	print ("\t\tUser   : " + replyname)
+	print ("\t\tMention: " + tweet.text)
+	print ("\t\tAnswer : " + reply + "\n\n")
+
+def drawing ():
+	line = ['_____\O/________/\________', '----{,_,">', '@( * O * )@', '-@-@-', '(-.-)Zzz...', 'c[_]']
+	return line [random.randrange (0, len(line))]
+
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = API(auth)
@@ -40,24 +49,40 @@ while True:
 	now = datetime.now()
 	tweets = api.search(q='@Bot_en_gel', since_id = IDS)
 
-	try
-		for tweet in tweets:
-			tweet.retweet()
+	for tweet in tweets:
+		try:	
+			if re.search ('^@Bot_en_gel', tweet.text):
+				tweet.retweet()
 
-			replyname = tweet.user.screen_name
-			sent = sentence()
-			reply = "@" + replyname + " " + sent
+				if (tweet.text == "@Bot_en_gel draw!"):
+					replyname = tweet.user.screen_name
+					sent = drawing ()
+					reply = "@" + replyname + " " + sent
 
-			api.update_status(status = reply)
+					api.update_status(status = reply)
 
-			print("Tweet contestado: " + reply)
-			write_id(tweet.id)
+					info_tweet (replyname, tweet.text, reply)
 
-	except TweepError as e:
-		print (e.reason)
+					write_id(tweet.id)
 
-	except StopIteration:
-		break
+				else:
+					replyname = tweet.user.screen_name
+					sent = sentence()
+					reply = "@" + replyname + " " + sent
+
+					api.update_status(status = reply)
+
+					info_tweet (replyname, tweet.text, reply)
+
+					write_id(tweet.id)
+
+		except TweepError as e:
+			print ("\n\n>>>\tUser: @" + tweet.user.screen_name + "   status: " +str (tweet.id) + "   time: " + str (tweet.created_at))
+			print (">>>\t" + tweet.text)
+			print (">>>\t" + e.reason)
+
+		except StopIteration:
+			break
 
 
 	if (now.hour % 2 == 0):
@@ -65,10 +90,11 @@ while True:
 	else:
 		word = 'Vainilla'
 	
-	tweet_data = word + ' ' + now.strftime("%H:%M") + '.'
+	tweet = word + ' ' + now.strftime("%H:%M") + '.'
 	api.update_status(status = tweet)
 	
 	log_tweet(run, now)
+	print ("--------------------------------------------------------------------------------")
 
-	time.sleep (1800)
 	run = run + 1
+	time.sleep (1800)
